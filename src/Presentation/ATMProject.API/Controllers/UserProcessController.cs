ad3ecda7a8736a506987232afd3ac4f39c1f007b
sahin <sahinuzun03@gmail.com>
2022-12-08 01:37:42 +0300

    Son Commit Düzenlemesi

--- src/Presentation/ATMProject.API/Controllers/UserProcessController.cs
+++ src/Presentation/ATMProject.API/Controllers/UserProcessController.cs
@@ -9,7 +9,7 @@ namespace ATMProject.API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    //[Authorize]
+    [Authorize]
     public class UserProcessController : ControllerBase
     {
         private readonly IUserService _userService;
